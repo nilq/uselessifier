c966ae90427edb7699e4764237087901cab55222
nilq <i.am@nilq.dk>
2016-09-23 22:10:44 +0200

    Add morse uselessifier

--- potato/src/morse/mod.rs
+++ potato/src/morse/mod.rs
@@ -1,7 +1,89 @@
+use std::collections::HashMap;
+
 pub struct Procedure;
 
 impl Procedure {
     pub fn do_stuff(data: &str) {
-        println!("Got this stuff: {}", data);
+        let mut morse_map = HashMap::new();
+        // alphabetic! - lowercase ...
+        morse_map.insert("a", "*-");
+        morse_map.insert("b", "-***");
+        morse_map.insert("c", "-*-*");
+        morse_map.insert("d", "-**");
+        morse_map.insert("e", "*");
+        morse_map.insert("f", "**-*");
+        morse_map.insert("g", "--*");
+        morse_map.insert("h", "****");
+        morse_map.insert("i", "**");
+        morse_map.insert("j", "*---");
+        morse_map.insert("k", "-*-");
+        morse_map.insert("l", "*-**");
+        morse_map.insert("m", "--");
+        morse_map.insert("n", "-*");
+        morse_map.insert("o", "---");
+        morse_map.insert("p", "*--*");
+        morse_map.insert("q", "--*-");
+        morse_map.insert("r", "*-*");
+        morse_map.insert("s", "***");
+        morse_map.insert("t", "-");
+        morse_map.insert("u", "**-");
+        morse_map.insert("v", "***-");
+        morse_map.insert("w", "*--");
+        morse_map.insert("x", "-**-");
+        morse_map.insert("y", "-*--");
+        morse_map.insert("z", "--**");
+        // alphabetic! - uppercase ...
+        morse_map.insert("A", "*-");
+        morse_map.insert("B", "-***");
+        morse_map.insert("C", "-*-*");
+        morse_map.insert("D", "-**");
+        morse_map.insert("E", "*");
+        morse_map.insert("F", "**-*");
+        morse_map.insert("G", "--*");
+        morse_map.insert("H", "****");
+        morse_map.insert("I", "**");
+        morse_map.insert("J", "*---");
+        morse_map.insert("K", "-*-");
+        morse_map.insert("L", "*-**");
+        morse_map.insert("M", "--");
+        morse_map.insert("N", "-*");
+        morse_map.insert("O", "---");
+        morse_map.insert("P", "*--*");
+        morse_map.insert("Q", "--*-");
+        morse_map.insert("R", "*-*");
+        morse_map.insert("S", "***");
+        morse_map.insert("T", "-");
+        morse_map.insert("U", "**-");
+        morse_map.insert("V", "***-");
+        morse_map.insert("W", "*--");
+        morse_map.insert("X", "-**-");
+        morse_map.insert("Y", "-*--");
+        morse_map.insert("Z", "--**");
+        // digits!
+        morse_map.insert("1", "*----");
+        morse_map.insert("2", "**---");
+        morse_map.insert("3", "***--");
+        morse_map.insert("4", "****-");
+        morse_map.insert("5", "*****");
+        morse_map.insert("6", "-****");
+        morse_map.insert("7", "--***");
+        morse_map.insert("8", "---**");
+        morse_map.insert("9", "----*");
+        morse_map.insert("0", "-----");
+        // space
+        morse_map.insert(" ", " ");
+
+        let split = data.split("");
+
+        let mut res = String::new();
+
+        for e in split {
+            match morse_map.get(e) {
+                Some(m) => res.push_str(m),
+                None => res.push_str(e),
+            }
+        }
+
+        println!("{}", res);
     }
 }
